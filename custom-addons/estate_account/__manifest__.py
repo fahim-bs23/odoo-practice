188a8a7b10b751ed981f653adb736d699e935767
fahim.bs23 <imtiaz.ahmed@brainstation-23.com>
2023-08-01 18:54:10 +0600

    add new module estate_account and generate invoice

---
+++ custom-addons/estate_account/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Estate_account',
+    'version': '1.0',
+    'category': 'Sales/CRM',
+    'sequence': 15,
+    'summary': 'Track leads and close opportunities',
+    'description': "",
+    'website': 'https://www.odoo.com/page/crm',
+    'depends': ['base','estate'],
+    'data': [
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
---
+++ custom-addons/estate_account/models/estate_property.py
@@ -0,0 +1,28 @@
+from odoo import models
+
+class EstateAccount(models.Model):
+    _inherit = "estate.property"
+
+    def action_sold(self):
+        res = super().action_sold()
+
+        for record in self:
+            move = self.env["account.move"].create(
+                {
+                    "partner_id": record.user_id,
+                    "move_type": "out_invoice",
+                    "invoice_line_ids": [
+                        (
+                            0,
+                            0,
+                            {
+                                "name": record.name,
+                                "quantity": 1.0,
+                                "price_unit": record.selling_price * 6.0 / 100.0,
+                            },
+                        ),
+                    ],
+                }
+            )
+        return res
+            
