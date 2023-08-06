e0daf1ad0d73f3726e8655c76b194550fdc44ce4
fahim.bs23 <imtiaz.ahmed@brainstation-23.com>
2023-08-06 18:50:54 +0600

    add Demo Data

--- custom-addons/estate/__manifest__.py
+++ custom-addons/estate/__manifest__.py
@@ -18,8 +18,11 @@
         'views/estate_property_offer_views.xml',
         'views/user_property_views.xml',
         'views/estate_menus.xml',
+        "data/demo_data.xml",
     ],
-    'demo': [],
+    'demo': [
+        
+    ],
     'installable': True,
     'application': True,
     'auto_install': False
